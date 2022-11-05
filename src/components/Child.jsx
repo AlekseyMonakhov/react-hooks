f13aa5aa6f9d435a906e238a20e715731ed47207
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-11-05 22:53:27 +0200

    upd

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 
 const Child = ({ returnComment }) => {
   const [counter, setCounter] = useState(1);
+  console.log("render");
   useEffect(() => {
     console.log("func was called");
   }, [returnComment]);
