e5ff94b6781d1d802c1621994d716e11d4208c30
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-03-25 22:31:31 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
 import "./App.css";
-import {useEffect, useLayoutEffect, useRef, useState} from "react";
+import Button from "./components/Button";
+import { useRef } from "react";
 
 function App() {
-    const [text, setText] = useState("1");
-    const inputRef = useRef(null);
-    useEffect(() => {
-        inputRef.current.value = "hello";
-        console.log(inputRef.current.value)
-    }, []);
-
-    useLayoutEffect(() => {
-        console.log(inputRef.current.value);
-    }, []);
-
-    const handleChange = (e) => {
-        setText(e.target.value)
-    }
-
-    const handleFocus = () => {
-        setText("")
-    }
-    return (
-        <div>
-            <input
-                ref={inputRef}
-                value={text}
-                onChange={handleChange}
-                onFocus={handleFocus}
-            ></input>
-        </div>
-    );
+  const ref = useRef(null);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "50%",
+        alignItems: "center",
+        margin: "0 auto",
+        gap: "10px",
+      }}
+    >
+      <button onClick={() => ref.current.alterToggler()}>
+        Button from parent
+      </button>
+      <Button ref={ref} />
+    </div>
+  );
 }
 
 export default App;
